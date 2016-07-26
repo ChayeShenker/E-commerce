5677ca9af636aeb7ed3bcd6ccf8a88ba1c3dc000
Chaye Shenker <chaybshenker@gmail.com>
2016-07-26 19:17:36 -0400

    Images

---
+++ ECommerce/Scripts/uplaodImage.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    var counter = 0;
+    $("#add-image").on("click", function () {
+        counter++;
+        $(".images").append("<input type='file' class='form-control'name='images[" + counter +"]'/>");
+    });
+});
